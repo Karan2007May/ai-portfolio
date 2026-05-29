fd3b7f42b1dbd7c0e88924ff586a59a9b6405a80
Karan Gattani <karangattani07@gmail.com>
2026-05-29 15:31:52 +0530

    Create project.js

---
+++ portfolio/api/project.js
@@ -0,0 +1,6 @@
+import projects from "../public/projects.json";
+
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).json(projects);
+}
